bb545518d971a9610c656dad28353c0f623897e5
wickyhien18 <giaphien1008@gmail.com>
2026-09-01 17:32:14 +0700

    update Status.py

--- Status.py
+++ Status.py
@@ -15,13 +15,11 @@ class Burn(StatusCondition):
     def on_turn_end(self, pokemon) -> None:
         dmg = pokemon.max_hp // 16
         pokemon.take_damage(dmg)
-        print(f"{pokemon.name} is Burn, lost {dmg} HP")
 
 class Poison(StatusCondition):
     def on_turn_end(self, pokemon) -> None:
         dmg = pokemon.max_hp // 8
         pokemon.take_damage(dmg)
-        print(f"{pokemon.name} is Poison, lost {dmg} HP")
 
 class Paralysis(StatusCondition):
     def on_apply(self, pokemon) -> None:
@@ -29,6 +27,5 @@ class Paralysis(StatusCondition):
 
     def on_turn_start(self, pokemon) -> bool:
         if random.random() < 0.25:
-            print(f"{pokemon.name} is Paralysis, can't move")
             return False
         return True
